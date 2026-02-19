72b38ccdb47e1e5e6cf4def8c3b585edee6b4ee9
Jakub K <u124_jakkie_waw@technischools.com>
2026-02-20 00:41:35 +0100

    test comit

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,19 @@ export default function App() {
   const [page, setPage] = useState<"home" | "cart" | "checkout">("home");
 
   return (
-      <div>
-        {/* NAVBAR PLACEHOLDER */}
-        <div style={{ padding: 20, borderBottom: "1px solid gray"}}>
-          <button onClick={() => setPage("home")}>Home</button>
-          <button onClick={() => setPage("cart")}>Cart</button>
-          <button onClick={() => setPage("checkout")}>Checkout</button>
-        </div>
-
-        <div>
-          {page === "home" && <Home />}
-          {page === "cart" && <Cart />}
-          {page === "checkout" && <Checkout />}
-        </div>
+    <div>
+      {/* NAVBAR PLACEHOLDER */}
+      <div style={{ padding: 20, borderBottom: "1px solid gray" }}>
+        <button onClick={() => setPage("home")}>Home</button>
+        <button onClick={() => setPage("cart")}>Cart</button>
+        <button onClick={() => setPage("checkout")}>Checkout</button>
       </div>
+
+      <div>
+        {page === "home" && <Home />}
+        {page === "cart" && <Cart />}
+        {page === "checkout" && <Checkout />}
+      </div>
+    </div>
   );
 }
